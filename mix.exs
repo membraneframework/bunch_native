790a7767525b9a0e30a6637e671c574f682b4649
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:25:31 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Bunch.Native.Mixfile do
 
   defp deps() do
     [
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:bundlex, "~> 1.0"}
     ]
   end
